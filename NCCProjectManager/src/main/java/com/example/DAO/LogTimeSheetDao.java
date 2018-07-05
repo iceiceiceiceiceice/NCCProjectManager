b13dae5705b761099e02c216909def8abec043d3
kduocnghi <kduocnghi@gmail.com>
2018-07-05 14:38:33 +0700

    fix lai filter
    hihi

--- NCCProjectManager/src/main/java/com/example/DAO/LogTimeSheetDao.java
+++ NCCProjectManager/src/main/java/com/example/DAO/LogTimeSheetDao.java
@@ -30,6 +30,6 @@ public interface LogTimeSheetDao{
 	public List<UserLogTimeSheetProjectWithoutIdDTO> findDataPaging(Integer from, Integer offset);
 	public BigInteger getCountLogTimeSheet();
 	public BigInteger test(String regx);
-	List<UserLogTimeSheetProjectWithoutIdDTO> findDataPagingFilter(String field,String value,int index_of_page);
+	List<UserLogTimeSheetProjectWithoutIdDTO> findDataPagingFilter(String field,String value,int index_of_page,int pageSize);
 	BigInteger countDataPagingFilter(String field,String value);
 }
