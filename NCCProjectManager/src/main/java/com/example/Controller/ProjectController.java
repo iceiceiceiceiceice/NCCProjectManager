e3abeba3c044b79a9bb92e6e12b75b1e945c9c2f
iceiceiceiceiceice <31066905+iceiceiceiceiceice@users.noreply.github.com>
2018-07-10 09:34:39 +0700

    ad

--- NCCProjectManager/src/main/java/com/example/Controller/ProjectController.java
+++ NCCProjectManager/src/main/java/com/example/Controller/ProjectController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,6 +58,11 @@ public class ProjectController {
 
 	}
 			
+	@GetMapping(value="/getAll")
+	@ResponseBody
+	public List<Project> getAll(){
+		return projectService.findAll();
+	}
 	
 	@PostMapping(value="/get-project")
 	@ResponseBody
@@ -83,14 +89,14 @@ public class ProjectController {
 	
 	@PostMapping(value=("/create-project"))
 	@ResponseBody
-	public Project addToProject(@RequestBody Project project) {
+	public createrProject addToProject(@RequestBody Project project) {
 
 		projectService.save(project);
 
 		createrProject cP =  new createrProject();
 		cP.setStatus(true);
 		cP.setProjectInfo(project);
-		return project;
+		return cP;
 
 	}
 
